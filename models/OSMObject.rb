1f4b7bf962e99274d9762b51c6a91dd002b319f9
Jennings Anderson <jennings.anderson@colorado.edu>
2014-09-23 12:00:23 -0600

    added more files

--- models/OSMObject.rb
+++ models/OSMObject.rb
@@ -4,4 +4,41 @@
 #
 #
 
+class OSMObject
 
+	attr_reader :id, :user_id, :user_name, :created_at
+
+	def initialize(args)
+		@id         = args[:id]
+		@user_id    = args[:user_id]
+		@user_name  = args[:user_name]
+		@created_at = args[:created_at]
+		@tags       = args[:tags]
+
+		post_initialize(args)
+	end
+
+	def post_initialize(args)
+		nil
+	end
+end
+
+class Node < OSMObject
+
+	attr_reader :lat, :lon
+
+	def initialize(args)  # Should this be post_initialize? What's the 
+		@lon = args[:lon] #  benefits/cons of super vs. post_initialize?
+		@lat = args[:lat]
+		super(args)
+	end
+end
+
+class Way < OSMObject
+
+	attr_reader :nodes
+
+	def initialize(args)
+		@nodes = args[:nodes]
+	end
+end
