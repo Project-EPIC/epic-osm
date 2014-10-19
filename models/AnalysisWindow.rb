dddd933a798ba9bff1368e88240b3f7a00805d36
Jennings Anderson <jennings.anderson@colorado.edu>
2014-10-18 18:51:13 -0600

    Everything works

--- models/AnalysisWindow.rb
+++ models/AnalysisWindow.rb
@@ -24,48 +24,159 @@ class AnalysisWindow
 		end
 	end
 
+	def build_buckets(unit=:all)
+		hour   = 60 * 60
+		day    = 24 * hour
+
+		buckets = []
+		
+		case unit
+		when :all
+			buckets << {start_date: time_frame.start, end_date: time_frame.end, objects: []}
+		
+		when :yearly
+			year = time_frame.start.year
+			bucket_start = Time.mktime(year, 1, 1)
+			while bucket_start < time_frame.end
+				bucket_end   = Time.mktime(year+=1, 1, 1)
+				buckets << {start_date: bucket_start, end_date: bucket_end, objects: []}
+				bucket_start = bucket_end
+			end
+
+		when :monthly
+			month = time_frame.start.mon
+			year  = time_frame.start.year
+			bucket_start = time_frame.start
+			while bucket_start < time_frame.end
+				bucket_start = Time.mktime( year, (month) )
+				if (month%12).zero?
+					year  += 1
+					month = 0
+				end
+				month+=1
+				bucket_end   = Time.mktime(year, (month) )
+				buckets << {start_date: bucket_start, end_date: bucket_end, objects: []}
+			end
+
+		when :daily
+			bucket_start = Time.mktime(time_frame.start.year, time_frame.start.mon, time_frame.start.day)
+			while bucket_start < time_frame.end
+				bucket_end   = Time.at( bucket_start.to_i + 1*day )
+				buckets << {start_date: bucket_start, end_date: bucket_end, objects: []}
+				bucket_start = bucket_end
+			end
+		
+		when :weekly
+			#fuck us, this is going to be ugly.  How should we do this? just start from the first week of the analysis window?
+			#We could just add 7 days.
+
+
+
+		when :hourly
+			bucket_start = Time.mktime(time_frame.start.year, time_frame.start.mon, time_frame.start.day, time_frame.start.hour)
+			while bucket_start < time_frame.end
+				bucket_end   = Time.at( bucket_start.to_i + 1*hour )
+				buckets << {start_date: bucket_start, end_date: bucket_end, objects: []}
+				bucket_start = bucket_end
+			end
+		end
+
+		buckets.first[:start_date] = time_frame.start
+		buckets.last[:end_date]    = time_frame.end
+
+		return buckets
+	end
+
+
+# Changesets
 	def changesets
-		@changesets ||= Changeset_Query.new(analysis_window: self).run
+		@changesets ||= Changeset_Query.new(analysis_window: self)
 	end
 
-	def changeset_count
-		changesets.count
+	def all_changesets
+		@all_changesets ||= changesets.all
 	end
 
-	def distinct_users_in_changesets
-		changesets.collect{|changeset| changeset.uid}.uniq
+	def changesets_per_year
+		@yearly_changesets ||= changesets.yearly
+	end
+
+	def changesets_per_month
+		@monthly_changesets ||= changesets.monthly
 	end
 
 	def changesets_per_day
-		changesets.group_by{|changeset| changeset.created_at.yday}
+		@daily_changesets ||= changesets.daily
 	end
 
-	def users_per_day
-		users_per_day = {}
-		changesets.group_by{|changeset| changeset.created_at.yday}.each do |k,v|
-			users_per_day[k] = v.collect{|changeset| changeset.uid}.uniq
-		end
-		users_per_day
+	def changeset_count
+		all_changesets.count
 	end
 
+	def distinct_users_in_changesets
+		all_changesets.collect{|changeset| changeset.uid}.uniq
+	end
+
+#Nodes
 	def nodes
-		@nodes ||= Node_Query.new(analysis_window: self).run
+		@nodes ||= Node_Query.new(analysis_window: self)
+	end
+
+	def all_nodes
+		@all_nodes ||= nodes.all
+	end
+
+	def monthly_nodes
+		@monthly_nodes ||= nodes.monthly
+	end
+
+	def daily_nodes
+		@daily_nodes ||= nodes.daily
 	end
 
 	def node_edit_count
-		nodes.count
+		all_nodes.count
 	end
 
 	def node_added_count
-		nodes.select{|node| node.version == 1}.count
+		all_nodes.select{|node| node.version == 1}.count
+	end
+
+#Users
+	def all_users_data
+		@all_users_data ||= User_Query.new(uids: distinct_users_in_changesets).run
+	end
+
+	def users_editing_per_year
+		years = {}
+		changesets_per_year.each do |bucket|
+			years[ bucket[:start_date] ] = bucket[:objects].collect{|changeset| changeset.user}.uniq
+		end
+		years
+	end
+
+	def users_editing_per_month
+		months = {}
+		changesets_per_month.each do |bucket|
+			months[ bucket[:start_date] ] = bucket[:objects].collect{|changeset| changeset.user}.uniq
+		end
+		months
 	end
 
 	def new_contributors
-		users = User_Query.new(analysis_window: self, uids: distinct_users_in_changesets).run
-
-		users.select{|user| user.account_created > time_frame.start and user.account_created < time_frame.end}.collect{|user| user.user}
+		all_users_data.select{|user| user.account_created > time_frame.start and user.account_created < time_frame.end}.collect{|user| user.user}
 	end
 
+	def top_contributors_by_changesets(args={limit: 5, unit: :all_time })
+
+		case args[:unit]
+		when :all_time
+			changesets_per_unit = all_changesets.group_by{|changeset| changeset.user}.sort_by{|k,v| v.length}.reverse
+		when :month
+			changesets_per_unit = changesets_per_month.group_by{|changeset| changeset.created_at.to_i / 100000}
+		end
+		changesets_per_unit.first(args[:limit])
+	end
 end
 
 
--- spec/questions_specs/changeset_questions_spec.rb
+++ spec/questions_specs/changeset_questions_spec.rb
@@ -1,44 +1,27 @@
 require "spec_helper"
 
-require_relative '../models/Query'
 
-
-describe Query do
+describe Changeset_Query do
 
   	it "Can count the distinct number of changesets for a given analysis window" do 
-
 		this_analysis_window = AnalysisWindow.new
-
 		puts "Number of Changesets: #{this_analysis_window.changeset_count}"
 
   	end
 
   	it "Can count the distinct users in changesets for a given analysis window" do 
-
 		this_analysis_window = AnalysisWindow.new
-
 		puts "Number of Distinct Users: #{this_analysis_window.distinct_users_in_changesets.length}"
   	end
 
-  	it "Can count the number of changesets per day" do 
+  	it "Can count the number of changesets per Month" do 
 		this_analysis_window = AnalysisWindow.new
 
-		per_day = this_analysis_window.changesets_per_day
+		per_month = this_analysis_window.changesets_per_month
 
-		puts "Day : Number of Changesets"
-		per_day.each do |k,v|
-			puts "#{k}:  #{v.length}"
-		end
-	end
-
-	it "Can count the number of users per day" do 
-		this_analysis_window = AnalysisWindow.new
-
-		per_day = this_analysis_window.users_per_day
-		
-		puts "Day : Number of Users"
-		per_day.each do |k,v|
-			puts "#{k}:  #{v.length}"
+		puts "Month : Number of Changesets"
+		per_month.each do |bucket|
+			puts "#{bucket[:start_date].year}-#{bucket[:start_date].mon}:  #{bucket[:objects].length}"
 		end
 	end
 end
