9d934b7068f9aea96a1388aa5231e60f203f882e
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-25 21:37:53 -0500

    changed logic on user questions

--- modules/questions/user_questions.rb
+++ modules/questions/user_questions.rb
@@ -31,10 +31,10 @@ module Questions # :nodoc: all
 			aw.all_users_data.each{ |user|
 				user_data.push({
 					"user" => user.user,
-					"nodes" => nodes_x_all.first[:objects].select{|node| node.uid == user.uid && ! node.tags.empty?}.count,
-					"ways" => ways_x_all.first[:objects].select{|way| way.uid == user.uid}.count,
-					"relations" => relations_x_all.first[:objects].select{|relation| relation.uid == user.uid}.count,
-					"changesets" => changesets_x_all.first[:objects].select{|changeset| changeset.uid == user.uid}.count,
+					"nodes" => nodes_x_all(constraints: {'uid' => user.uid}).first[:objects].select{ |node| !node.tags.empty?}.count,
+					"ways" => ways_x_all(constraints: {'uid' => user.uid}).first[:objects].count,
+					"relations" => relations_x_all(constraints: {'uid' => user.uid}).first[:objects].count,
+					"changesets" => changesets_x_all(constraints: {'uid' => user.uid}).first[:objects].count,
 				})
 			}
 			user_data
@@ -47,11 +47,11 @@ module Questions # :nodoc: all
 				user_data[ user.user ] = {
 					"type" => "FeatureCollection",
 					"features" =>
-						ways_x_all.first[:objects].select{|way| way.uid == user.uid}.collect{|way|  
-							{ "type" => "Feature", "properties"=> way.tags, "geometry" => way.geometry } 
+						ways_x_all(constraints: {'uid' => user.uid}).first[:objects].collect{|way|
+							{ "type" => "Feature", "properties"=> way.tags, "geometry" => way.geometry }
 						} +
-						nodes_x_all.first[:objects].select{|node| node.uid == user.uid && ! node.tags.empty?}.collect{|node|  
-							{ "type" => "Feature", "properties"=> node.tags, "geometry" => node.geometry } 
+						nodes_x_all(constraints: {'uid' => user.uid}).first[:objects].select{|node| !node.tags.empty?}.collect{|node|
+							{ "type" => "Feature", "properties"=> node.tags, "geometry" => node.geometry }
 						}
 				}
 			}
@@ -65,10 +65,10 @@ module Questions # :nodoc: all
 			when :all_time
 				changesets_per_unit = aw.changesets_x_all.first[:objects].group_by{|changeset| changeset.user}.sort_by{|k,v| v.length}.reverse
 			when :month
-				changesets_per_unit = aw.changesets_x_month.group_by{ |changeset| 
-					if defined? changeset.created_at 
+				changesets_per_unit = aw.changesets_x_month.group_by{ |changeset|
+					if defined? changeset.created_at
 						changeset.created_at.to_i / 100000
-					end			
+					end
 				}
 			end
 			changesets_per_unit.first(args[:limit])
