13d29b5e41d4f8db30d732969b48db99a5ae18c1
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-26 19:13:21 -0600

    updated the network module

--- epic-osm.rb
+++ epic-osm.rb
@@ -105,7 +105,7 @@ class EpicOSM
 
 	def run_network_functions
 		network_info = aw_config['temporal_network']
-			temp = TemporalAnalysis.new(aw: analysis_window, step: network_info['step'], unit: network_info['unit'], directory: network_info['files'])
+			temp = Questions::Networks::TemporalAnalysis.new(aw: analysis_window, step: network_info['step'], unit: network_info['unit'], directory: network_info['files'])
 			temp.run_overlapping_changesets
 	end
 
