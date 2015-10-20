d13908e699110b847a601200f7b98e805ba07266
Jennings Anderson <jennings.anderson@colorado.edu>
2015-10-19 23:24:34 -0600

    upping

--- src/main/scala/EpicDriver.scala
+++ src/main/scala/EpicDriver.scala
@@ -3,12 +3,33 @@
 import EpicOSM._
 import EpicTwitter._
 
+import java.nio.file.{Files,Path,Paths}
+import java.io.{File,PrintWriter}
+
+
 object EpicDriver {
 
   def main(args: Array[String]) {
 
     println("Hello World")
 
+    //Lets import some tweets
+    val tweet_file = Paths.get("test/sandy_tweets.json").toAbsolutePath.normalize
+    val tweets = get_tweets(tweet_file)
+
+   	val tweet_objs = tweets.map{t => EpicTwitter.parseJsonToTweet(t)}
+
+   	tweet_objs.take(10).foreach(println)
+
+  }
+
+  //Based on github.com/kenbod/scala_parse_json
+
+  private def get_tweets(input: Path) = {
+    val handle = io.Source.fromFile(input.toFile)
+    val data   = handle.mkString
+    handle.close // close input file
+    data.split("\n")
   }
 
 }
--- src/main/scala/EpicTwitter.scala
+++ src/main/scala/EpicTwitter.scala
@@ -1,20 +1,28 @@
 import spray.json._
 import DefaultJsonProtocol._
 
-
 //The simplest case class: A Tweet
 case class SimpleTweet(
 	handle: 	String, 
 	time: 		String, 
 	text: 		String, 
+	context:    Boolean,
 	geo: 		Array[Float]
-	)
+)
+
 object SimpleTweet
 
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val tweetFormat = jsonFormat5(SimpleTweet.apply)
+}
 
-
-
+import MyJsonProtocol._
 
 object EpicTwitter {
+
+	def parseJsonToTweet(jsonString: String)={
+    	val jsonAst = jsonString.parseJson
+    	jsonAst.convertTo[SimpleTweet] 
+	}
 
 }
