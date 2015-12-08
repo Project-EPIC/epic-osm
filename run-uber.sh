5ba615554089c0f21355b2691e533a5195254b9c
Jennings Anderson <jennings.anderson@colorado.edu>
2015-12-08 16:22:16 -0700

    deleting run scripts that were brokeN

--- run-uber.sh
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-./gradlew clean && \
-./gradlew shadowJar && \
-=======
-./gradlew jar
->>>>>>> 9577702199ae603cba1b45dedaacc508bc75ea2f
-scala -classpath build/libs/epic-spark-all.jar EpicDriver
