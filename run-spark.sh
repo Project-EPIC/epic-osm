c01127473cc0d04246c6445e5f1de7a3f9f4f6a1
Jennings Anderson <jennings.anderson@colorado.edu>
2015-12-10 21:40:16 -0700

    upping

--- run-spark.sh
+++
@@ -1,3 +0,0 @@
-./gradlew clean && \
-./gradlew shadowJar && \
-/usr/local/Cellar/apache-spark/1.5.2/bin/spark-shell --jars build/libs/spark-tweets-all.jar
--- run.sh
+++
@@ -1,4 +0,0 @@
-gradle clean
-gradle jar && \
-scala -cp "dependencies/spray-json.jar:build/libs/spark-tweets.jar" edu.colorado.cs.epic.SparkTweets
-
